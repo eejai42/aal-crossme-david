f1a3036555e9da69f38c5fcac9191af914f6b9e0
davidgandjui <kgandjui@gmail.com>
2022-03-16 15:58:32 -0500

    python work
    started working in python

---
+++ DavidToEjAirtablexml/pythonthing.py
@@ -0,0 +1,12 @@
+print("I Made This Print 'Hello World' Thing Work So I Am Now Moving To New Things ")
+
+print("\n0001000"
+      "\n0111110"
+      "\n1111111"
+      "\n0001000"
+      "\n0001000"
+      "\n0001000"
+      "\n0011000"
+      "\n0011000")
+
+
